0ab9b8b9e3f8bce4e2b88227fc8646163224ba03
SSOragneCATY <jumaoz@qq.com>
2024-07-07 22:14:51 +0800

    修改法杖的最大堆叠数

--- src/main/java/net/archasmiel/thaumcraft/item/TCItemRegister.java
+++ src/main/java/net/archasmiel/thaumcraft/item/TCItemRegister.java
@@ -4,6 +4,7 @@ import net.archasmiel.thaumcraft.Thaumcraft;
 import net.archasmiel.thaumcraft.ThaumcraftTabs;
 import net.archasmiel.thaumcraft.core.item.TCItem;
 import net.archasmiel.thaumcraft.core.wands.WandRod;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -13,7 +14,7 @@ import java.util.function.Supplier;
 public class TCItemRegister {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Thaumcraft.MODID);
 
-    public static final DeferredItem<Item> staff_rod_blaze = register("staff_rod_blaze", () -> new WandRod(new TCItem.TCProperties.Builder(new Item.Properties()).build(), 50));
+    public static final DeferredItem<Item> staff_rod_blaze = register("staff_rod_blaze", () -> new WandRod(new TCItem.TCProperties.Builder(new Item.Properties().component(DataComponents.MAX_STACK_SIZE,1)).build(), 50));
 
     public static <T extends Item> DeferredItem<T> register(String name, Supplier<T> item) {
         DeferredItem<T> i = ITEMS.register(name, item);
